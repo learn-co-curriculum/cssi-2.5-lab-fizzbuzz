07a8f3ba24330f5b29668550f422655d33feb4cc
crepric <crepric@gmail.com>
2015-07-14 16:32:46 -0500

    Comment about parseInt() in code snippet.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function renderFizzBuzz(){
   input =  $('input').val();
+  // parseInt() takes a string representing a number
+  // as input and converts it into a numerical value.
   $('#results').text(fizzbuzz(parseInt(input)));
 }
 
